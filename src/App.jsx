84f39010621fc705d472bc13d025607052cee1eb
Konstantin Teterin <teterinkonst@gmail.com>
2025-02-21 01:45:40 +0300

    ready module for testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,84 @@
-import { useState } from 'react';
-import { createElement } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
-// Декларативный код
+import { useState } from "react";
+import "./App.css";
+import styles from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+	const [value, setValue] = useState("");
+	const [list, setList] = useState([]);
+	const [error, setError] = useState("");
+	const [isValueValid, setIsValueValid] = useState(false);
 
-  const viteLogoLink = createElement(
-    'a',
-    { href: 'https://vite.dev', target: '_blank' },
-    createElement('img', {
-      src: `${viteLogo}`,
-      className: 'logo',
-      alt: 'Vite logo',
-    }),
-  );
+	const onInputButtonClick = () => {
+		const promptValue = prompt();
+		if (promptValue.length < 3) {
+			setError("Введенное значение должно содержать минимум 3 символа");
+			setValue("");
+			setIsValueValid(false);
+		} else {
+			setValue(promptValue);
+			setError("");
+			setIsValueValid(true);
+		}
+	};
 
-  const reactLogoLink = createElement(
-    'a',
-    { href: 'https://react.dev', target: '_blank' },
-    createElement('img', {
-      src: `${reactLogo}`,
-      className: 'logo react',
-      alt: 'React logo',
-    }),
-  );
+	const onAddButtonClick = () => {
+		if (value) {
+			setList((list) => [...list, { id: Date.now(), value }]);
+			setValue("");
+			setError("");
+		}
+	};
 
-  const divLogos = createElement('div', {}, [viteLogoLink, reactLogoLink]);
-
-  const h1 = createElement('h1', {}, 'Vite + React');
-
-  const buttonCounter = createElement(
-    'button',
-    {
-      onClick: () => setCount((count) => count + 1),
-    },
-    `count is ${count}`,
-  );
-
-  const reminderEdit = createElement(
-    'p',
-    {},
-    'Edit <code>src/App.jsx</code> and save to test HMR',
-  );
-
-  const divCard = createElement('div', { className: 'card' }, [
-    buttonCounter,
-    reminderEdit,
-  ]);
-
-  const readTheDocs = createElement(
-    'p',
-    { className: 'read-the-docs' },
-    'Click on the Vite and React logos to learn more',
-  );
-
-  const currentYear = createElement(
-    'div',
-    {},
-    `${new Date().getFullYear()} год`,
-  );
-
-  return (
-    <>
-      {divLogos} {h1} {divCard} {readTheDocs} {currentYear}
-    </>
-  );
+	return (
+		<>
+			<div className={styles.app}>
+				<h1 className={styles.pageHeading}>Ввод значения</h1>
+				<p className={styles.noMarginText}>
+					Текущее значение <code>value</code>: "
+					<output className={styles.currentValue}>{value}</output>"
+				</p>
+				{error !== "" && <div className={styles.error}>{error}</div>}
+				<div className={styles.buttonsContainer}>
+					<button
+						className={styles.button}
+						onClick={onInputButtonClick}
+					>
+						Ввести новое
+					</button>
+					<button
+						className={styles.button}
+						disabled={!isValueValid}
+						onClick={onAddButtonClick}
+					>
+						Добавить в список
+					</button>
+				</div>
+				<div className={styles.listContainer}>
+					<h2 className={styles.listHeading}>Список:</h2>
+					{list.length ? (
+						<ul className={styles.list}>
+							{list.map(({ id, value }) => {
+								return (
+									<li className={styles.listItem} key={id}>
+										{value} (Дата и время создания:{" "}
+										{new Date(id)
+											.toLocaleString("ru-RU")
+											.split(",")
+											.join("")}
+										)
+									</li>
+								);
+							})}
+						</ul>
+					) : (
+						<p className={styles.noMarginText}>
+							Нет добавленных элементов
+						</p>
+					)}
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
